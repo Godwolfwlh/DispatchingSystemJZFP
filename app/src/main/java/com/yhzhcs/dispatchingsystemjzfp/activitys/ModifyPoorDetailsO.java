8f1487754791ca965dd4e63f1137c86a6851cd68
LuhuaiWu <godwolfwlh@163.com>
2018-03-06 14:12:34 +0800

    添加编辑贫困户信息页面

--- app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/activitys/ModifyPoorDetailsO.java
+++ app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/activitys/ModifyPoorDetailsO.java
@@ -3,6 +3,9 @@ package com.yhzhcs.dispatchingsystemjzfp.activitys;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.yhzhcs.dispatchingsystemjzfp.R;
 
@@ -10,10 +13,40 @@ import com.yhzhcs.dispatchingsystemjzfp.R;
  * Created by Administrator on 2018/3/6.
  */
 
-public class ModifyPoorDetailsO extends AppCompatActivity {
+public class ModifyPoorDetailsO extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView titleImgL;
+    private TextView titleName;
+    private ImageView titleImgR;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.modify_poor_details);
+        intView();
+    }
+
+    private void intView(){
+        titleImgL = (ImageView) findViewById(R.id.title_left);
+        titleName = (TextView) findViewById(R.id.title_name);
+        titleImgR = (ImageView) findViewById(R.id.title_right);
+
+        titleImgL.setVisibility(View.VISIBLE);
+        titleImgL.setImageResource(R.mipmap.icon_back);
+        titleName.setText("基本情况");
+        titleImgR.setImageResource(R.mipmap.icon_sure_1);
+        titleImgL.setOnClickListener(this);
+        titleImgR.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_left:
+                finish();
+                break;
+            case R.id.title_right:
+                break;
+        }
     }
 }
