ccb6d99307dd43ec2feb134d9a328addec106734
Luhuai Wu <godwolfwlh@163.com>
2018-03-14 15:15:12 +0800

    GridView长按多选功能实现，选择图片删除

--- app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/fragments/ImgFragment.java
+++ app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/fragments/ImgFragment.java
@@ -1,5 +1,6 @@
 package com.yhzhcs.dispatchingsystemjzfp.fragments;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -18,10 +19,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.lidroid.xutils.BitmapUtils;
 import com.lidroid.xutils.HttpUtils;
 import com.lidroid.xutils.exception.HttpException;
 import com.lidroid.xutils.http.RequestParams;
@@ -30,7 +37,6 @@ import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.yhzhcs.dispatchingsystemjzfp.R;
 import com.yhzhcs.dispatchingsystemjzfp.activitys.SampleCameraActivity;
-import com.yhzhcs.dispatchingsystemjzfp.adapters.PoorImageAdapter;
 import com.yhzhcs.dispatchingsystemjzfp.bean.Inglists;
 import com.yhzhcs.dispatchingsystemjzfp.bean.PoorImageBean;
 import com.yhzhcs.dispatchingsystemjzfp.utils.Constant;
@@ -41,6 +47,7 @@ import com.yhzhcs.dispatchingsystemjzfp.utils.TypeConverter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -50,7 +57,7 @@ import static android.app.Activity.RESULT_OK;
  * Created by Administrator on 2018/1/24.
  */
 
-public class ImgFragment extends Fragment implements View.OnClickListener {
+public class ImgFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private View v;
 
@@ -60,11 +67,17 @@ public class ImgFragment extends Fragment implements View.OnClickListener {
 
     private List<Inglists> listBean;
 
-    private TextView Upload, Delete;
+    private TextView Upload, Select, Cancel, Eliminate;
+    private ImageView titleImage, titlePng;
+    private Button butDeleta;
 
     public final int TYPE_TAKE_PHOTO = 1;//Uri获取类型判断
     public final int CODE_TAKE_PHOTO = 1;//相机RequestCode
     public Uri photoUri;
+
+    private ArrayList<Boolean> selectItems; //用于存储已选中项目的位置
+    private boolean isState;
+    private PoorImageAdapter adapter;
 
     @Nullable
     @Override
@@ -96,7 +109,7 @@ public class ImgFragment extends Fragment implements View.OnClickListener {
                 Gson gson = new Gson();
                 PoorImageBean poorImageBean = gson.fromJson(body, PoorImageBean.class);
                 listBean = poorImageBean.getIngLists();
-                PoorImageAdapter adapter = new PoorImageAdapter(listBean, getActivity());
+                adapter = new PoorImageAdapter(listBean, getActivity());
 
                 gridView.setAdapter(adapter);
             }
@@ -110,16 +123,30 @@ public class ImgFragment extends Fragment implements View.OnClickListener {
 
     private void iniView() {
         gridView = (GridView) v.findViewById(R.id.img_grid_view);
+        titleImage = (ImageView) v.findViewById(R.id.img_title_image);
+        Cancel = (TextView) v.findViewById(R.id.img_title_content);
         Upload = (TextView) v.findViewById(R.id.img_title_text);
-        Delete = (TextView) v.findViewById(R.id.img_title_delete);
+        titlePng = (ImageView) v.findViewById(R.id.img_title_png_1);
+        Select = (TextView) v.findViewById(R.id.img_title_select);
+        butDeleta = (Button) v.findViewById(R.id.btn_del);
+        Eliminate = (TextView) v.findViewById(R.id.img_title_eliminate);
 
         Upload.setOnClickListener(this);
-        Delete.setOnClickListener(this);
+        Cancel.setOnClickListener(this);
+        Select.setOnClickListener(this);
+        butDeleta.setOnClickListener(this);
+        gridView.setOnItemClickListener(this);
+        gridView.setOnItemLongClickListener(this);
+        Eliminate.setOnClickListener(this);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    public ArrayList<Boolean> getSelectItems() {
+        return selectItems;
     }
 
     @Override
@@ -129,12 +156,135 @@ public class ImgFragment extends Fragment implements View.OnClickListener {
             case R.id.img_title_text:
                 showListDialog();
                 break;
-            case R.id.img_title_delete:
+            case R.id.img_title_select:
+                setSelectAll(true);
+                break;
+            case R.id.img_title_content:
+                selectItems.clear();
+                adapter.setIsState(false);
+                setState(false);
+                break;
+            case R.id.img_title_eliminate:
+                setSelectAll(false);
+                break;
+            case R.id.btn_del:
                 break;
         }
 
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        if (isState) {
+            CheckBox checkBox = (CheckBox) view.findViewById(R.id.check_box);
+            if (checkBox.isChecked()) {
+                checkBox.setChecked(false);
+                selectItems.set(position, false);
+            } else {
+                checkBox.setChecked(true);
+                selectItems.set(position, true);
+            }
+            adapter.notifyDataSetChanged();
+            setSelectNum();
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+        if (!isState) {
+            selectItems = new ArrayList<>();
+            for (int i = 0; i < listBean.size(); i++) {
+                selectItems.add(false);
+            }
+            CheckBox box = (CheckBox) view.findViewById(R.id.check_box);
+            box.setChecked(true);
+            selectItems.set(position, true);
+            setState(true);
+            adapter.setIsState(true);
+            setSelectNum();
+        }
+        return false;
+    }
+
+    //设置当前状态 是否在多选模式
+    private void setState(boolean b) {
+        isState = b;
+        if (b) {
+            titleImage.setVisibility(View.GONE);
+            Cancel.setText("取消");
+            Cancel.setOnClickListener(this);
+            Select.setVisibility(View.VISIBLE);
+            titlePng.setVisibility(View.VISIBLE);
+            butDeleta.setVisibility(View.VISIBLE);
+        } else {
+            titleImage.setVisibility(View.VISIBLE);
+            Cancel.setText("图片管理");
+            Select.setVisibility(View.GONE);
+            titlePng.setVisibility(View.GONE);
+            butDeleta.setVisibility(View.GONE);
+        }
+    }
+
+    //显示已选项数目
+    private void setSelectNum() {
+        int num = 0;
+        for (Boolean b : selectItems) {
+            if (b)
+                num++;
+        }
+        LogUtil.v("setselectnum", "已选择" + num + "项");
+    }
+
+    //全选
+    private void setSelectAll(boolean b) {
+        for (int i = 0; i < selectItems.size(); i++) {
+            selectItems.set(i, b);
+            adapter.notifyDataSetChanged();
+            setSelectNum();
+        }
+        Select.setVisibility(b ? View.GONE : View.VISIBLE);
+        Eliminate.setVisibility(b ? View.VISIBLE : View.GONE);
+    }
+
+    //删除
+    private void delSelections() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        if (!selectItems.contains(true)) {
+            builder.setTitle("提示").setMessage("当前未选中项目").setPositiveButton("确认", null).create().show();
+        } else {
+            builder.setTitle("提示");
+            builder.setMessage("确认删除所选项目？");
+            builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    for (int i = 0; i < listBean.size(); i++) {
+                        if (selectItems.get(i)) {
+                            listBean.set(i, null);
+                        }
+                    }
+                    while (listBean.contains(null)) {
+                        listBean.remove(null);
+                    }
+                    selectItems = new ArrayList<>();
+                    for (int i = 0; i < listBean.size(); i++) {
+                        selectItems.add(false);
+                    }
+
+                    adapter.setData(listBean);
+                    adapter.notifyDataSetChanged();
+                    if (listBean.size() == 0) {
+                        adapter.setIsState(false);
+                        setState(false);
+                        return;
+                    }
+                }
+            });
+            builder.setNegativeButton("取消", null);
+            builder.create().show();
+        }
+
+    }
 
     private void showListDialog() {
         final String[] items = {"拍照", "相册"};
@@ -260,15 +410,98 @@ public class ImgFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onSuccess(ResponseInfo<String> responseInfo) {
                 LogUtil.v("SAMPLE_CAMERA_HTTP", "onSuccess：" + responseInfo.result.toString());
-                ToastUtil.showInfo(getActivity(),"上传成功！");
+                ToastUtil.showInfo(getActivity(), "上传成功！");
             }
 
             @Override
             public void onFailure(HttpException e, String s) {
                 LogUtil.v("SAMPLE_CAMERA_HTTP", "onFailure：" + s);
-                ToastUtil.showInfo(getActivity(),"上传失败！");
+                ToastUtil.showInfo(getActivity(), "上传失败！");
             }
         });
     }
 
+    public class PoorImageAdapter extends BaseAdapter {
+        private Context context;
+
+        private List<Inglists> listBean;
+        private BitmapUtils bitmapUtils;
+
+        private boolean isState = false;
+
+        public PoorImageAdapter(List<Inglists> listBean, Context context) {
+            super();
+            this.listBean = listBean;
+            this.context = context;
+        }
+
+        public void setData(List<Inglists> listBean) {
+            this.listBean = listBean;
+        }
+
+        public void setIsState(boolean isState) {
+            this.isState = isState;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return listBean != null ? listBean.size() : 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listBean.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder viewHolder = null;
+            Inglists inglists = listBean.get(position);
+            if (convertView == null) {
+
+                viewHolder = new ViewHolder();
+                // 获得容器
+                convertView = LayoutInflater.from(this.context).inflate(R.layout.grid_view_item, null);
+
+                // 初始化组件
+                viewHolder.image = (ImageView) convertView.findViewById(R.id.image_grid_item);
+                viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.check_box);
+                // 给converHolder附加一个对象
+                convertView.setTag(viewHolder);
+            } else {
+                // 取得converHolder附加的对象
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            // 给组件设置资源
+            bitmapUtils = new BitmapUtils(context);    //创建BitmapUtils对象，通过xUtils框架获取
+            bitmapUtils.configDefaultBitmapConfig(Bitmap.Config.RGB_565);   //设置图片清晰度
+            bitmapUtils.display(viewHolder.image, inglists.getAnnexPath());
+
+            viewHolder.checkBox.setVisibility(isState ? View.VISIBLE : View.GONE);
+
+            ArrayList<Boolean> list = getSelectItems();
+            if (selectItems != null) {
+                LogUtil.v("selectItemssssssssss", "selectItems==2==" + selectItems.size());
+                if (selectItems.size() != 0) {
+                    LogUtil.v("selectItemssssssssss", "selectItems==3==" + selectItems.get(position).toString());
+                    viewHolder.checkBox.setChecked(selectItems.get(position));
+                }
+            }
+            return convertView;
+        }
+
+        class ViewHolder {
+            public ImageView image;
+            public CheckBox checkBox;
+        }
+
+    }
 }
