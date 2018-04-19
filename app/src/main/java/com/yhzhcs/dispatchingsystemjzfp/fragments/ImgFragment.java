d19e368fe0057d43d6f5111915b8574572fcee49
Luhuai Wu <godwolfwlh@163.com>
2018-04-19 11:16:41 +0800

    调试项目

--- app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/fragments/ImgFragment.java
+++ app/src/main/java/com/yhzhcs/dispatchingsystemjzfp/fragments/ImgFragment.java
@@ -27,8 +27,6 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -54,7 +52,6 @@ import com.yhzhcs.dispatchingsystemjzfp.utils.TypeConverter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -64,7 +61,7 @@ import static android.app.Activity.RESULT_OK;
  * Created by Administrator on 2018/1/24.
  */
 
-public class ImgFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+public class ImgFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemLongClickListener {
 
     private View v;
 
@@ -74,18 +71,14 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
 
     private List<Inglists> listBean;
 
-    private TextView Upload, Select, Cancel, Eliminate;
-    private ImageView titleImage, titlePng;
-    private Button butDeleta;
+    private TextView Upload, Cancel, butDeleta;
 
     public final int TYPE_TAKE_PHOTO = 1;//Uri获取类型判断
     public final int CODE_TAKE_PHOTO = 1;//相机RequestCode
     private static final int RESULT_PICK = 201;
     public Uri photoUri;
-
-    private ArrayList<Boolean> selectItems; //用于存储已选中项目的位置
-    private boolean isState;
     private PoorImageAdapter adapter;
+    private boolean isShowDelete = false;
 
     private String annentId, annexPathDown;
 
@@ -189,30 +182,18 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
 
     private void iniView() {
         gridView = (GridView) v.findViewById(R.id.img_grid_view);
-        titleImage = (ImageView) v.findViewById(R.id.img_title_image);
         Cancel = (TextView) v.findViewById(R.id.img_title_content);
         Upload = (TextView) v.findViewById(R.id.img_title_text);
-        titlePng = (ImageView) v.findViewById(R.id.img_title_png_1);
-        Select = (TextView) v.findViewById(R.id.img_title_select);
-        butDeleta = (Button) v.findViewById(R.id.btn_del);
-        Eliminate = (TextView) v.findViewById(R.id.img_title_eliminate);
+        butDeleta = (TextView) v.findViewById(R.id.img_title_delete);
 
         Upload.setOnClickListener(this);
-        Cancel.setOnClickListener(this);
-        Select.setOnClickListener(this);
         butDeleta.setOnClickListener(this);
-        gridView.setOnItemClickListener(this);
         gridView.setOnItemLongClickListener(this);
-        Eliminate.setOnClickListener(this);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    public ArrayList<Boolean> getSelectItems() {
-        return selectItems;
     }
 
     @Override
@@ -222,159 +203,34 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
             case R.id.img_title_text:
                 showListDialog();
                 break;
-            case R.id.img_title_select:
-                setSelectAll(true);
-                break;
-            case R.id.img_title_content:
-                selectItems.clear();
-                adapter.setIsState(false);
-                setState(false);
-                break;
-            case R.id.img_title_eliminate:
-                setSelectAll(false);
-                break;
-            case R.id.btn_del:
-                delSelections();
-                break;
-        }
-
-    }
-
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        if (isState) {
-            CheckBox checkBox = (CheckBox) view.findViewById(R.id.check_box);
-            if (checkBox.isChecked()) {
-                checkBox.setChecked(false);
-                selectItems.set(position, false);
-            } else {
-                checkBox.setChecked(true);
-                selectItems.set(position, true);
-            }
-            adapter.notifyDataSetChanged();
-            setSelectNum();
-        }
-    }
-
-    @Override
-    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-
-        if (!isState) {
-            selectItems = new ArrayList<>();
-            for (int i = 0; i < listBean.size(); i++) {
-                selectItems.add(false);
-            }
-            CheckBox box = (CheckBox) view.findViewById(R.id.check_box);
-            box.setChecked(true);
-            selectItems.set(position, true);
-            setState(true);
-            adapter.setIsState(true);
-            setSelectNum();
-        }
-        return false;
-    }
-
-    //设置当前状态 是否在多选模式
-    private void setState(boolean b) {
-        isState = b;
-        if (b) {
-            titleImage.setVisibility(View.GONE);
-            Cancel.setText("取消");
-            Cancel.setOnClickListener(this);
-            Select.setVisibility(View.VISIBLE);
-            titlePng.setVisibility(View.VISIBLE);
-            butDeleta.setVisibility(View.VISIBLE);
-        } else {
-            titleImage.setVisibility(View.VISIBLE);
-            Cancel.setText("图片管理");
-            Select.setVisibility(View.GONE);
-            titlePng.setVisibility(View.GONE);
-            butDeleta.setVisibility(View.GONE);
-        }
-    }
-
-    //显示已选项数目
-    private void setSelectNum() {
-        int num = 0;
-        for (Boolean b : selectItems) {
-            if (b)
-                num++;
-        }
-        LogUtil.v("setselectnum", "已选择" + num + "项");
-    }
-
-    //全选
-    private void setSelectAll(boolean b) {
-        for (int i = 0; i < selectItems.size(); i++) {
-            selectItems.set(i, b);
-            adapter.notifyDataSetChanged();
-            setSelectNum();
-        }
-        Select.setVisibility(b ? View.GONE : View.VISIBLE);
-        Eliminate.setVisibility(b ? View.VISIBLE : View.GONE);
-    }
-
-    //删除
-    private void delSelections() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        if (!selectItems.contains(true)) {
-            builder.setTitle("提示").setMessage("当前未选中项目").setPositiveButton("确认", null).create().show();
-        } else {
-            builder.setTitle("提示");
-            builder.setMessage("确认删除所选项目？");
-            builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    for (int i = 0; i < listBean.size(); i++) {
-                        if (selectItems.get(i)) {
-                            deletePhoto(listBean);
-                            listBean.set(i, null);
-                        }
-                    }
-                    while (listBean.contains(null)) {
-                        listBean.remove(null);
-                    }
-                    selectItems = new ArrayList<>();
-                    for (int i = 0; i < listBean.size(); i++) {
-                        selectItems.add(false);
-                    }
-
-                    adapter.setData(listBean);
-                    adapter.notifyDataSetChanged();
-                    if (listBean.size() == 0) {
-                        adapter.setIsState(false);
-                        setState(false);
-                        return;
-                    }
+            case R.id.img_title_delete:
+                if (isShowDelete) {
+                    isShowDelete = false;
+                } else {
+                    isShowDelete = true;
                 }
-            });
-            builder.setNegativeButton("取消", null);
-            builder.create().show();
+                isMultiselect(isShowDelete);
+                adapter.setIsState(isShowDelete);
+                break;
         }
 
     }
 
-    //删除服务器文件
-    private void deletePhoto(List<Inglists> data) {
-        HttpUtils httpUtils = new HttpUtils();
-        RequestParams params = new RequestParams();
-        for (int i = 0; i < data.size(); i++) {
-            annentId = listBean.get(i).getAnnentid();
-            annexPathDown = listBean.get(i).getAnnexPathDown();
-            params.addBodyParameter("annentId", annentId);
-            params.addBodyParameter("annexPathDown", annexPathDown);
-        }
-        httpUtils.send(HttpMethod.POST, Constant.URL_DELETE_PHOTO, params, new RequestCallBack<String>() {
-            @Override
-            public void onSuccess(ResponseInfo<String> responseInfo) {
-                LogUtil.v("DELETEPHOTOHTTP", "onSuccess：" + responseInfo.result);
-            }
+    //是否在多选状态
+    private void isMultiselect(boolean isShowDelete) {
+        butDeleta.setText(isShowDelete ? "取消" : "删除");
+    }
 
-            @Override
-            public void onFailure(HttpException e, String s) {
-                LogUtil.v("DELETEPHOTOHTTP", "onFailure：" + s);
-            }
-        });
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (isShowDelete) {
+            isShowDelete = false;
+        } else {
+            isShowDelete = true;
+        }
+        isMultiselect(isShowDelete);
+        adapter.setIsState(isShowDelete);
+        return true;
     }
 
     private void showListDialog() {
@@ -421,9 +277,9 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
         }
     }
 
-    private void accessToAlbumRights(){
+    private void accessToAlbumRights() {
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(),new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, RESULT_PICK);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, RESULT_PICK);
 
         } else {
             autoObtainAlbumPermission();
@@ -451,7 +307,7 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
         }
     }
 
-    private void autoObtainAlbumPermission(){
+    private void autoObtainAlbumPermission() {
         Bundle bundle = new Bundle();
         Intent intent = new Intent(getActivity(), SampleCameraActivity.class);
         bundle.putString("entityId", entityId);
@@ -570,13 +426,11 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
         });
     }
 
-
     public class PoorImageAdapter extends BaseAdapter {
         private Context context;
 
         private List<Inglists> listBean;
         private BitmapUtils bitmapUtils;
-
         private boolean isState = false;
 
         public PoorImageAdapter(List<Inglists> listBean, Context context) {
@@ -622,7 +476,7 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
 
                 // 初始化组件
                 viewHolder.image = (ImageView) convertView.findViewById(R.id.image_grid_item);
-                viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.check_box);
+                viewHolder.delete = (ImageView) convertView.findViewById(R.id.delete_image);
                 // 给converHolder附加一个对象
                 convertView.setTag(viewHolder);
             } else {
@@ -635,22 +489,37 @@ public class ImgFragment extends Fragment implements View.OnClickListener, Adapt
             bitmapUtils.configDefaultBitmapConfig(Bitmap.Config.RGB_565);   //设置图片清晰度
             bitmapUtils.display(viewHolder.image, inglists.getAnnexPath());
 
-            viewHolder.checkBox.setVisibility(isState ? View.VISIBLE : View.GONE);
+            viewHolder.delete.setVisibility(isState ? View.VISIBLE : View.GONE);
 
-            ArrayList<Boolean> list = getSelectItems();
-            if (selectItems != null) {
-                LogUtil.v("selectItemssssssssss", "selectItems==2==" + selectItems.size());
-                if (selectItems.size() != 0) {
-                    LogUtil.v("selectItemssssssssss", "selectItems==3==" + selectItems.get(position).toString());
-                    viewHolder.checkBox.setChecked(selectItems.get(position));
-                }
-            }
             return convertView;
+        }
+
+        //删除服务器文件
+        private void deletePhoto(List<Inglists> data) {
+            HttpUtils httpUtils = new HttpUtils();
+            RequestParams params = new RequestParams();
+            for (int i = 0; i < data.size(); i++) {
+                annentId = listBean.get(i).getAnnentid();
+                annexPathDown = listBean.get(i).getAnnexPathDown();
+                params.addBodyParameter("annentId", annentId);
+                params.addBodyParameter("annexPathDown", annexPathDown);
+            }
+            httpUtils.send(HttpMethod.POST, Constant.URL_DELETE_PHOTO, params, new RequestCallBack<String>() {
+                @Override
+                public void onSuccess(ResponseInfo<String> responseInfo) {
+                    LogUtil.v("DELETEPHOTOHTTP", "onSuccess：" + responseInfo.result);
+                }
+
+                @Override
+                public void onFailure(HttpException e, String s) {
+                    LogUtil.v("DELETEPHOTOHTTP", "onFailure：" + s);
+                }
+            });
         }
 
         class ViewHolder {
             public ImageView image;
-            public CheckBox checkBox;
+            public ImageView delete;
         }
 
     }
